2aba0b2161155d14755ec51653e1b1f041c18a0a
maydanikas <123328755+maydanikas@users.noreply.github.com>
2023-04-03 17:23:58 +0000

    Deploying to gh-pages from @ maydanikas/goit-js-hw-09@0967e56e1da91f401685bff127e9730bb0ebcc56 🚀

--- 01-color-switcher.91f8399f.js
+++
@@ -1,2 +0,0 @@
-!function(){var t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]"),n=document.body;t.addEventListener("click",(function(){setInterval((function(){t.disabled=!0,n.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0))}),1e3)})),e.addEventListener("click",(function(){clearInterval(colorSwitch),t.disabled=!1}))}();
-//# sourceMappingURL=01-color-switcher.91f8399f.js.map
---
+++ 01-color-switcher.b050c352.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]"),a=document.body;let d;t.addEventListener("click",(()=>{d=setInterval((()=>{t.disabled=!0,a.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`}),1e3)})),e.addEventListener("click",(()=>{clearInterval(d),t.disabled=!1}));
+//# sourceMappingURL=01-color-switcher.b050c352.js.map
