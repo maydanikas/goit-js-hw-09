6af176ec234e14c3093bf4d3c750e1bcb3ba6074
maydanikas <123328755+maydanikas@users.noreply.github.com>
2023-04-05 19:54:44 +0200

    task 3 promises generator

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,45 @@
+import Notiflix from 'notiflix';
+
+const formEl = document.querySelector('.form');
+
+////////////Func declaration create promise instanse & randomise result//
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+  return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
+}
+
+function onFormElSubmit(event) {
+  event.preventDefault();
+  console.log('form submitted');
+  ////////taking data from input form/////////////////////
+  const delay = +event.target.elements.delay.value;
+  console.log('delay is:', delay);
+  const step = +event.target.elements.step.value;
+  console.log('step is:', step);
+  const amount = +event.target.elements.amount.value;
+  console.log('amount is: ', amount);
+  ///////loop creates instanses //////////////////////////
+  for (let i = 1; i <= amount; i += 1) {
+    createPromise(i, delay + (i - 1) * step)
+      .then(({ position, delay }) => {
+        Notiflix.Notify.success(
+          `✅ Fulfilled promise ${position} in ${delay}ms`
+        );
+      })
+      .catch(({ position, delay }) => {
+        Notiflix.Notify.failure(
+          `❌ Rejected promise ${position} in ${delay}ms`
+        );
+      });
   }
 }
+
+formEl.addEventListener('submit', onFormElSubmit);
