6ecd62afb334da758dfb7c6a49db5b7647440c49
maydanikas <123328755+maydanikas@users.noreply.github.com>
2023-04-06 10:28:34 +0200

    task 1 fixed

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -2,8 +2,7 @@ const startBtnEl = document.querySelector('[data-start]');
 const stopBtnEl = document.querySelector('[data-stop]');
 const bodyEl = document.body;
 
-// console.log(getRandomHexColor());
-let colorSwitch;
+let loopColorSwitch;
 
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
@@ -11,14 +10,17 @@ function getRandomHexColor() {
     .padStart(6, 0)}`;
 }
 
-startBtnEl.addEventListener('click', () => {
-  colorSwitch = setInterval(() => {
+const onStartColorChange = () => {
+  loopColorSwitch = setInterval(() => {
     startBtnEl.disabled = true;
     bodyEl.style.backgroundColor = getRandomHexColor();
   }, 1000);
-});
+};
 
-stopBtnEl.addEventListener('click', () => {
-  clearInterval(colorSwitch);
+const onStopColorChange = () => {
+  clearInterval(loopColorSwitch);
   startBtnEl.disabled = false;
-});
+};
+
+startBtnEl.addEventListener('click', onStartColorChange);
+stopBtnEl.addEventListener('click', onStopColorChange);
